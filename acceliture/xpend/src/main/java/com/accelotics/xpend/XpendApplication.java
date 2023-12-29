3052567b32c0662ca5e7bc0b195b7f6d37565e08
bhavesh-asana <asanabhavesh@gmail.com>
2023-12-29 14:21:07 -0600

    spring app for budget tracking

---
+++ acceliture/xpend/src/main/java/com/accelotics/xpend/XpendApplication.java
@@ -0,0 +1,15 @@
+package com.accelotics.xpend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpendApplication.class, args);
+
+		System.out.println("Running Successfully!!");
+	}
+
+}
---
+++ acceliture/xpend/src/test/java/com/accelotics/xpend/XpendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelotics.xpend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
